4fac13b51932d8e30f1f16f4dd990989b5f67aee
mertugrulfrontenddev <166544633+mertugrulfrontenddev@users.noreply.github.com>
2024-11-01 00:07:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ boxes.forEach((box) => {
     singleClickTimer = setTimeout(() => {
       if (lastBox !== box && firstBox !== box) {
         if (!box.classList.contains("up") && !box.classList.contains("down")) {
-          box.classList.add("up");
-          box.parentElement.classList.toggle("active");
+          
+          box.parentElement.classList.add("active");
         } else {
-          box.classList.remove("up");
+          
+          box.parentElement.classList.remove("active");
         }
       } else {
         if (lastBox === box) {
